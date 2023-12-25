781c352bb38cbb09bfb7b3f2c331979c0345c329
xmartina <martinsprince126@gmail.com>
2023-12-25 18:29:15 +0100

    init

--- users/dashboard/index.php
+++ users/dashboard/index.php
@@ -6,37 +6,13 @@ ini_set('display_errors', '1');
 
 // Start the session
 session_start();
-if (isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id'])) {
     header("Location: dashboard.php");
     exit();
+} elseif (isset($_SESSION['user_id'])){
+    require_once ('all-nft/index.php');
 }
 
-
-// Check if 'user_id' is set in the URL parameters
-if (isset($_GET['user_id'])) {
-    // Get the value of 'user_id'
-    $user_id = $_GET['user_id'];
-
-    // Validate if it's a valid number
-    if (is_numeric($user_id)) {
-        // Assign it to a session variable
-        $_SESSION['user_id'] = $user_id;
-
-        // Continue with your code or redirect to the desired location
-//        header("Location: /user/dashboard");
-        require_once ('all-nft/index.php');
-        exit();
-    } else {
-        // Handle the case where 'user_id' is not a valid number
-        header("Location:".siteUrl. "users/auth/login");
-        exit();
-    }
-} else {
-    // Handle the case where 'user_id' is not set in the URL
-//    header("Location: /auth?login");
-    header("Location:".siteUrl. "users/auth/login");
-    exit();
-}
 // Display dashboard content
 
 ?>
