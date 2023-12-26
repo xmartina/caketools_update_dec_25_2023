323578fb17997d69f55828f071e6f5e9f2d64ada
xmartina <martinsprince126@gmail.com>
2023-12-26 23:57:13 +0100

    d

--- users/dashboard/index.php
+++ users/dashboard/index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-if (isset($_SESSION['user_id'])) {
-    require_once ('all-nft/index.php');
-    exit();
-} else{
-    header("Location: /users/auth/login");
-    exit();
-}
+//if (isset($_SESSION['user_id'])) {
+//    require_once ('all-nft/index.php');
+//    exit();
+//} else{
+//    header("Location: /users/auth/login");
+//    exit();
+//}
 echo $_SESSION['user_id'];
