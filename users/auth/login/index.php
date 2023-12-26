4482d06f8a47238e2624caf91f5ec75561b9394e
xmartina <martinsprince126@gmail.com>
2023-12-26 06:57:48 +0100

    d

--- users/auth/login/index.php
+++ users/auth/login/index.php
@@ -1,79 +1,156 @@
-<?php error_reporting(E_ALL);
-ini_set('display_errors', 1);
-const siteUrl = 'https://caketoolnftmarketplace.com/';
-const rootDir = '/home/multistream6/domains/caketoolnftmarketplace.com/public_html/';
+<?php
+    const rootDir = '/home/multistream6/domains/caketoolnftmarketplace.com/public_html/';
+    include_once (rootDir.'users/auth/partials/header/login_header.php');
 ?>
-<!-- HTML form for login -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap 4 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/users/auth/auth.css">
-    <?php
-    session_start();
-    if (isset($_SESSION['user_id'])) {
-        header("Location:" .siteUrl. "users/dashboard");
-        exit();
-    }
 
-    require_once (rootDir.'users/auth/header.php');?>
-    <title>User Login</title>
-</head>
+<div class="position-relative">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner py-4">
+            <!-- Login -->
+            <div class="card p-2">
+                <!-- Logo -->
+                <div class="app-brand justify-content-center mt-5">
+                    <a href="/" class="app-brand-link gap-2">
+                <span class="app-brand-logo demo">
+                  <span style="color: #666cff">
+                    <svg width="268" height="150" viewBox="0 0 38 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                          d="M30.0944 2.22569C29.0511 0.444187 26.7508 -0.172113 24.9566 0.849138C23.1623 1.87039 22.5536 4.14247 23.5969 5.92397L30.5368 17.7743C31.5801 19.5558 33.8804 20.1721 35.6746 19.1509C37.4689 18.1296 38.0776 15.8575 37.0343 14.076L30.0944 2.22569Z"
+                          fill="currentColor" />
+                      <path
+                          d="M30.171 2.22569C29.1277 0.444187 26.8274 -0.172113 25.0332 0.849138C23.2389 1.87039 22.6302 4.14247 23.6735 5.92397L30.6134 17.7743C31.6567 19.5558 33.957 20.1721 35.7512 19.1509C37.5455 18.1296 38.1542 15.8575 37.1109 14.076L30.171 2.22569Z"
+                          fill="url(#paint0_linear_2989_100980)"
+                          fill-opacity="0.4" />
+                      <path
+                          d="M22.9676 2.22569C24.0109 0.444187 26.3112 -0.172113 28.1054 0.849138C29.8996 1.87039 30.5084 4.14247 29.4651 5.92397L22.5251 17.7743C21.4818 19.5558 19.1816 20.1721 17.3873 19.1509C15.5931 18.1296 14.9843 15.8575 16.0276 14.076L22.9676 2.22569Z"
+                          fill="currentColor" />
+                      <path
+                          d="M14.9558 2.22569C13.9125 0.444187 11.6122 -0.172113 9.818 0.849138C8.02377 1.87039 7.41502 4.14247 8.45833 5.92397L15.3983 17.7743C16.4416 19.5558 18.7418 20.1721 20.5361 19.1509C22.3303 18.1296 22.9391 15.8575 21.8958 14.076L14.9558 2.22569Z"
+                          fill="currentColor" />
+                      <path
+                          d="M14.9558 2.22569C13.9125 0.444187 11.6122 -0.172113 9.818 0.849138C8.02377 1.87039 7.41502 4.14247 8.45833 5.92397L15.3983 17.7743C16.4416 19.5558 18.7418 20.1721 20.5361 19.1509C22.3303 18.1296 22.9391 15.8575 21.8958 14.076L14.9558 2.22569Z"
+                          fill="url(#paint1_linear_2989_100980)"
+                          fill-opacity="0.4" />
+                      <path
+                          d="M7.82901 2.22569C8.87231 0.444187 11.1726 -0.172113 12.9668 0.849138C14.7611 1.87039 15.3698 4.14247 14.3265 5.92397L7.38656 17.7743C6.34325 19.5558 4.04298 20.1721 2.24875 19.1509C0.454514 18.1296 -0.154233 15.8575 0.88907 14.076L7.82901 2.22569Z"
+                          fill="currentColor" />
+                      <defs>
+                        <linearGradient
+                            id="paint0_linear_2989_100980"
+                            x1="5.36642"
+                            y1="0.849138"
+                            x2="10.532"
+                            y2="24.104"
+                            gradientUnits="userSpaceOnUse">
+                          <stop offset="0" stop-opacity="1" />
+                          <stop offset="1" stop-opacity="0" />
+                        </linearGradient>
+                        <linearGradient
+                            id="paint1_linear_2989_100980"
+                            x1="5.19475"
+                            y1="0.849139"
+                            x2="10.3357"
+                            y2="24.1155"
+                            gradientUnits="userSpaceOnUse">
+                          <stop offset="0" stop-opacity="1" />
+                          <stop offset="1" stop-opacity="0" />
+                        </linearGradient>
+                      </defs>
+                    </svg>
+                  </span>
+                </span>
+                        <span class="app-brand-text demo text-heading fw-bold">Materialize</span>
+                    </a>
+                </div>
+                <!-- /Logo -->
 
-<body>
-<div class="main-form d-flex align-items-center justify-content-center">
-    <div class="form-wrap d-flex align-items-center flex-column px-4">
-        <h2 class="pt-5 pb-3">User Login
-            <?php
-            if (isset($_GET['wrong-pass'])) {
-                echo '<div class="error-msg text-danger">
-                            your password is incorrect, please check and try again
-                          </div>';
-            } elseif (isset($_GET['no-user'])) {
-                echo '<div class="error-msg text-danger">
-                            Sorry this user was not found, please check and try again
-                          </div>';
-            } elseif (isset($_GET['login-success'])){
-                echo '<div class="success-msg text-success">
-                            login successful, <a href="/user/dashboard">redirecting to dashboard</a>
-                          </div>';
-            }
-            ?>
-        </h2>
-        <form method="post" action="">
-            <div onclick="location.href='https://caketoolnftmarketplace.com/users/auth/register'" class="have-acct rounded d-flex align-items-center justify-content-evenly bg-primary text-white"><div class="icon-g"><span class="material-symbols-outlined px-3">person</span></div>
-                Register An Account</div>
-            <div class="pt-3"></div>
-            <div class="form-group">
-                <div class="row ">
-                    <div class="col-lg-12">
-                        <label for="">Email or Username
-                            <input type="text" name="user_input" required>
-                        </label>
+                <div class="card-body mt-2">
+                    <h4 class="mb-2">Welcome to Materialize! 👋</h4>
+                    <p class="mb-4">Please sign-in to your account and start the adventure</p>
+
+                    <form id="formAuthentication" class="mb-3" action="index.html" method="GET">
+                        <div class="form-floating form-floating-outline mb-3">
+                            <input
+                                type="text"
+                                class="form-control"
+                                id="email"
+                                name="email-username"
+                                placeholder="Enter your email or username"
+                                autofocus />
+                            <label for="email">Email or Username</label>
+                        </div>
+                        <div class="mb-3">
+                            <div class="form-password-toggle">
+                                <div class="input-group input-group-merge">
+                                    <div class="form-floating form-floating-outline">
+                                        <input
+                                            type="password"
+                                            id="password"
+                                            class="form-control"
+                                            name="password"
+                                            placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                            aria-describedby="password" />
+                                        <label for="password">Password</label>
+                                    </div>
+                                    <span class="input-group-text cursor-pointer"><i class="mdi mdi-eye-off-outline"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-3 d-flex justify-content-between">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="remember-me" />
+                                <label class="form-check-label" for="remember-me"> Remember Me </label>
+                            </div>
+                            <a href="auth-forgot-password-basic.html" class="float-end mb-1">
+                                <span>Forgot Password?</span>
+                            </a>
+                        </div>
+                        <div class="mb-3">
+                            <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                        </div>
+                    </form>
+
+                    <p class="text-center">
+                        <span>New on our platform?</span>
+                        <a href="auth-register-basic.html">
+                            <span>Create an account</span>
+                        </a>
+                    </p>
+
+                    <div class="divider my-4">
+                        <div class="divider-text">or</div>
+                    </div>
+
+                    <div class="d-flex justify-content-center gap-2">
+                        <a href="javascript:;" class="btn btn-icon btn-lg rounded-pill btn-text-facebook">
+                            <i class="tf-icons mdi mdi-24px mdi-facebook"></i>
+                        </a>
+
+                        <a href="javascript:;" class="btn btn-icon btn-lg rounded-pill btn-text-twitter">
+                            <i class="tf-icons mdi mdi-24px mdi-twitter"></i>
+                        </a>
+
+                        <a href="javascript:;" class="btn btn-icon btn-lg rounded-pill btn-text-github">
+                            <i class="tf-icons mdi mdi-24px mdi-github"></i>
+                        </a>
+
+                        <a href="javascript:;" class="btn btn-icon btn-lg rounded-pill btn-text-google-plus">
+                            <i class="tf-icons mdi mdi-24px mdi-google"></i>
+                        </a>
                     </div>
                 </div>
             </div>
-            <div class="form-group">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <label for="">Password
-                            <input type="password" name="password" required>
-                        </label>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="row">
-                    <div class="col-8">
-                        <input type="submit" value="Login" name="login">
-                    </div>
-                </div>
-            </div>
-        </form>
+            <!-- /Login -->
+            <img
+                alt="mask"
+                src="../../assets/img/illustrations/auth-basic-login-mask-light.png"
+                class="authentication-image d-none d-lg-block"
+                data-app-light-img="illustrations/auth-basic-login-mask-light.png"
+                data-app-dark-img="illustrations/auth-basic-login-mask-dark.png" />
+        </div>
     </div>
 </div>
-<?php require_once(rootDir.'users/auth/footer.php'); ?>
 
+<?php
+    include_once (rootDir.'users/auth/partials/footer/login_footer.php');
+?>
